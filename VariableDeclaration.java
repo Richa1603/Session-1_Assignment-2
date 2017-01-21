79afd8ea9d472bd3fdf6f4a002c795a5e1073c81
Richa1603 <richa_kautoo@hotmail.com>
2017-01-21 02:55:27 -0800

    Add files via upload

---
+++ VariableDeclaration.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package acadgildassignment;
+
+/**
+ *
+ * @author Richa
+ */
+public class VariableDeclaration {
+    
+    public static void main(String[] args)
+        {
+                
+        int a = 20;
+        int b = 10;
+      // int b = (a--) - (--a); 
+       System.out.println("INT b = " +((a--)-(--a)));
+        System.out.println("INT c = " +(a--));
+        System.out.println("INT d = " +(a>>2));
+        System.out.println("INT e = " +(a&b));
+       // int b= a-- - --a;
+//int c=a--;
+//int d=a>>2;
+//int e=a&b;
+
+    
+}
